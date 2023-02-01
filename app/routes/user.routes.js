38751ded0a9df390c2cbb9dde80e91c262690947
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-01-31 21:31:02 -0500

    adding user.routes file for the route configuration for this assignment

---
+++ app/routes/user.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const auth = require('../auth/auth.js')
+
+const userController = require("../controllers/user.controller.js");
+
+var router = require("express").Router();
+
+router.post("/user", userController.create);
+
+router.get("/user/:id" ,auth, userController.getById);
+
+router.put("/user/:id" ,auth, userController.updateById);
+
+module.exports = router;  
