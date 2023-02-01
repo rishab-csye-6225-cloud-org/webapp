5a437c5e9da0cd371768d653d8a41529bd97472a
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-01-31 21:33:20 -0500

    adding authentication file as a middleware inorder to check the authentication

---
+++ app/auth/auth.js
@@ -0,0 +1,61 @@
+const { userModel } = require("../models/index.js");
+const bcrypt = require("bcrypt");
+const User = userModel;
+
+
+const Auth = async (req, res, next) => {
+    
+    // validate authorization header is present
+    if (!req.headers.authorization || req.headers.authorization.indexOf('Basic ') === -1) {
+        return res.status(401).json({
+            message: 'Authorization header is missing'
+        })
+    }
+
+    // validate authentication credentials
+    const base64 = req.headers.authorization.split(' ')[1];
+    const credential = Buffer.from(base64, 'base64').toString('ascii');
+    const [email, password] = credential.split(':');
+
+    //if after split one of the fields if empty then throw error
+    if(email=="" || password=="")
+    {
+
+        return res.status(401).json({
+            message: 'Email or password is missing/empty!'
+        })
+
+    }
+
+    
+    const user = await User.findOne({ 
+        where: { username: email } 
+    });
+    if (!user) {
+        return res.status(400).json({
+            message: 'Bad Request by the User'
+        })
+    }
+    
+    // verify password
+    const isPasswordMatch = bcrypt.compareSync(password, user.password)
+    if (!isPasswordMatch) {
+        return res.status(401).json({
+            message: 'Not a Valid Authentication Credentials'
+        })
+    }
+    // validate if user is trying to access his personal account
+    if (req.params.id){
+        if (user.id !== parseInt(req.params.id)) {
+            return res.status(403).json({
+                message: 'Forbidden Resource!!'
+            })
+        }
+    }
+
+    //success and the other call to the next callback function
+    next();
+
+}
+
+module.exports = Auth;
