ab9e8cc0a0cbb4059cc573a1b0c11b9645ffa18b
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-02-20 18:44:17 -0500

    adding packer for ami build and changes in workflow file

---
+++ scripts/start.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+sudo yum update
+sudo yum upgrade -y
+
+echo "Inside the AMI Machine"
+
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+node -v
+
+
+ls -a
+sudo amazon-linux-extras enable postgresql14
+sudo yum install postgresql-server -y
+sudo postgresql-setup initdb
+#sudo PGSETUP_INITDB_OPTIONS=" --auth=trust" postgresql-setup --initdb --unit postgresql --debug
+
+sudo sed -i 's/ident/md5/g' /var/lib/pgsql/data/pg_hba.conf
+
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo systemctl status postgresql
+sudo -u postgres psql
+sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Rishab16';"
+
+echo "password changed"
+
+echo "made changes in the conf file"
+
+sudo cat /var/lib/pgsql/data/pg_hba.conf
+
+#sudo systemctl restart postgresql
+
+
+ls
+mkdir webapp
+cp /tmp/webapp.zip /home/ec2-user/webapp/webapp.zip
+
+#giving permissions to folder where webapp is copied to read, write, and execute to all 
+#owner , onwe-group and outside world as well
+sudo chmod -R 777 /home/ec2-user/webapp/   
+
+echo "Permissions on the webapp configured"
+
+cd /home/ec2-user/webapp/
+unzip webapp.zip -d /home/ec2-user/webapp/
+pwd
+cd webapp/
+
+#setting env variables so that the application can use it
+# export DB_HOSTNAME=localhost
+# export DB_PASSWORD=Rishab16
+# export DB_USER=postgres
+# export DB_NAME=postgres
+# export DB_PORT=5432
+# export APP_PORT=9000
+
+npm i
+npm test
+
+
+
+
