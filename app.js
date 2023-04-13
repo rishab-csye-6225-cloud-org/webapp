b0e9cf3cdfd7a5b6679b4c88d098df05a3f55b60
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-04-13 13:08:29 -0400

    testing workflow 2

--- app.js
+++ app.js
@@ -40,18 +40,6 @@ app.get("/healthz" , (req,res) =>{
     
 })
 
-//cicd
-app.get("/cicd" , (req,res) =>{
-    try{
-        client.increment('get.cicd');
-        logger.info("Requested healthz point : /cicd");
-        return res.status(200).send();
-    }catch(err){
-        logger.error("Something went wrong -> healthz point : /healthz");
-        return res.status(400).json(err); 
-    }
-    
-})
 
 
 module.exports = app;
