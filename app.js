3f46e4f0f2a39d61452e8365f8eb5a05794f25d9
Rishab Rajesh Agarwal <agarwal.risha@northeastern.edu>
2023-04-12 22:26:56 -0400

    testing 2

--- app.js
+++ app.js
@@ -41,13 +41,13 @@ app.get("/healthz" , (req,res) =>{
 })
 
 //cicd test
-app.get("/cicd" , (req,res) =>{
+app.get("/abc" , (req,res) =>{
     try{
-        client.increment('get.cicd');
-        logger.info("Requested cicd point : /cicd");
+        client.increment('get.abc');
+        logger.info("Requested abc point : /abc");
         return res.status(200).send();
     }catch(err){
-        logger.error("Something went wrong -> cicd point : /cicd");
+        logger.error("Something went wrong -> abc point : /abc");
         return res.status(400).json(err);
     }
     
